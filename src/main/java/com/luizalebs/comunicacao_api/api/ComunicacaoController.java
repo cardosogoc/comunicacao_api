bc744cd2fff34d4e4aae5e68edbac323fe5faa83
Gabriel Cardoso <gabrieloc6@gmail.com>
2026-08-24 15:13:55 -0300

    primeiro commit

---
+++ src/main/java/com/luizalebs/comunicacao_api/api/ComunicacaoController.java
@@ -0,0 +1,31 @@
+package com.luizalebs.comunicacao_api.api;
+
+import com.luizalebs.comunicacao_api.api.dto.ComunicacaoInDTO;
+import com.luizalebs.comunicacao_api.api.dto.ComunicacaoOutDTO;
+import com.luizalebs.comunicacao_api.business.service.ComunicacaoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/comunicacao")
+@RequiredArgsConstructor
+public class ComunicacaoController {
+
+    private final ComunicacaoService service;
+
+    @PostMapping("/agendar")
+    public ResponseEntity<ComunicacaoOutDTO> agendar(@RequestBody ComunicacaoInDTO dto)  {
+        return ResponseEntity.ok(service.agendarComunicacao(dto));
+    }
+
+    @GetMapping()
+    public ResponseEntity<ComunicacaoOutDTO> buscarStatus(@RequestParam String emailDestinatario) {
+        return ResponseEntity.ok(service.buscarStatusComunicacao(emailDestinatario));
+    }
+
+    @PatchMapping("/cancelar")
+    public ResponseEntity<ComunicacaoOutDTO> cancelarStatus(@RequestParam String emailDestinatario) {
+        return ResponseEntity.ok(service.alterarStatusComunicacao(emailDestinatario));
+    }
+}
